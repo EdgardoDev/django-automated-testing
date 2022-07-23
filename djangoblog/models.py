865e2e93c6db032c2fa4ac26253d8b2b3b9c4003
EdgardoDev <info@edgardopintoescalier.com>
2022-07-23 16:27:08 +0200

    Added Python code quality checks with PEP 8/FLake

--- djangoblog/models.py
+++ djangoblog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -7,4 +8,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-
--- djangoblog/tests.py
+++ djangoblog/tests.py
@@ -3,13 +3,14 @@ from . models import Post
 
 # We start by building some small tests
 
+
 class ModelTest(TestCase):
 
     def setUp(self):
-        self.djangoblog = Post.objects.create(title="Django Automated Testing", author="Edgardo", slug="django-automated-testing")
-    
+        self.djangoblog = Post.objects.create(title="Django Automated Testing", author="Edgardo",
+                                              slug="django-automated-testing")
+
     def test_post_model(self):
         djangoblogtest = self.djangoblog
-        self.assertTrue(isinstance(djangoblogtest, Post)) # If data goes into the db assert True.
+        self.assertTrue(isinstance(djangoblogtest, Post))  # If data goes into the db assert True.
         self.assertEqual(str(djangoblogtest), "Django Automated Testing")
-
