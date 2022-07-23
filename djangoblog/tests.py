1bd77bc540bfaf61ce06cc35acede1986d1c2bd0
EdgardoDev <info@edgardopintoescalier.com>
2022-07-23 15:08:49 +0200

    Added a simple unit test using coverage

--- djangoblog/tests.py
+++ djangoblog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from . models import Post
 
-# Create your tests here.
+# We start by building some small tests
+
+class ModelTest(TestCase):
+
+    def setUp(self):
+        self.djangoblog = Post.objects.create(title="Django Automated Testing", author="Edgardo", slug="django-automated-testing")
+    
+    def test_post_model(self):
+        djangoblogtest = self.djangoblog
+        self.assertTrue(isinstance(djangoblogtest, Post)) # If data goes into the db assert True.
+        self.assertEqual(str(djangoblogtest), "Django Automated Testing")
+
